4c4a0351f081f14ecd9a84aca480afb8bce27401
Matt Kraus <matt.kraus@nuvolect.com>
2017-02-24 14:10:45 -0500

    Updated copyright to include legal@nuvolect.com for use beyond GPLv3.

--- SecureSuite/src/main/java/com/nuvolect/securesuite/main/CloudImportDialog.java
+++ SecureSuite/src/main/java/com/nuvolect/securesuite/main/CloudImportDialog.java
@@ -1,18 +1,23 @@
 /*
  * Copyright (c) 2017. Nuvolect LLC
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * Contact legal@nuvolect.com for a less restrictive commercial license if you would like to use the
+ * software without the GPLv3 restrictions.
  *
- * You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.  If not,
+ * see <http://www.gnu.org/licenses/>.
  *
  */
 
 package com.nuvolect.securesuite.main;
-//
-//TODO create class description
-//
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -25,6 +30,10 @@ import com.nuvolect.securesuite.util.LogUtil;
 import com.nuvolect.securesuite.util.Persist;
 import com.nuvolect.securesuite.util.WorkerCommand;
 
+
+/**
+ * Cloud account import dialogs.
+ */
 public class CloudImportDialog {
 
     private static CharSequence[] m_importAccountList;
